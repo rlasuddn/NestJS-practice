520cf69844f954c01b008ccd10b3c178e7c14559
rlasuddn <ywaves073@gmail.com>
2023-05-06 22:27:19 +0900

    feat: TypeORM MySQL 연동 및 UserEntity 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,10 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { EmailService } from './email/email.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    UsersModule,
     ConfigModule.forRoot({
       envFilePath:
         process.env.NODE_ENV === 'production'
@@ -17,6 +17,17 @@ import { ConfigModule } from '@nestjs/config';
           : '.env',
       isGlobal: true,
     }), // 개발환경에 따라 ConfigModule 동적 생성
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql', //데이터베이스 타입
+      host: process.env.DB_HOST, //데이터베이스 호스트 주소
+      port: 3306, //포트 번호 기본 3306
+      username: process.env.DB_USER,
+      password: process.env.DB_PW,
+      database: 'nestjs_practice', //연결할 데이터 베이스 스키마 이름
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.DB_SYNC === 'true',
+    }),
   ],
   controllers: [AppController],
   providers: [AppService, EmailService],
